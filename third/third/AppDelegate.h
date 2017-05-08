ecf73bab9ee896f78b9a11ab425a6f772f9781ad
Huiyu Zou <460085271@qq.com>
2017-05-08 16:08:05 +0800

    第二次

---
+++ third/third/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  third
+//
+//  Created by Huiyu Zou on 17/5/8.
+//  Copyright © 2017年 huiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ third/third/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  third
+//
+//  Created by Huiyu Zou on 17/5/8.
+//  Copyright © 2017年 huiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
